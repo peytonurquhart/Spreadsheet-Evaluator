ea62b5648eca6df693e879e6c07cf14250748c48
Peyton <peyton.urquhart@wsu.edu>
2020-04-16 14:19:56 -0700

    HW9 initial commit

---
+++ Spreadsheet_Tests/XML_Reading_TestClass.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="XML_Reading_TestClass.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Spreadsheet_Tests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Drawing;
+    using CptS321;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test class for reading XML documents using .NET libraries.
+    /// </summary>
+    [TestFixture]
+    public class XML_Reading_TestClass
+    {
+    }
+}
